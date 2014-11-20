46a77ceb7a92f60f345a3f18be9b238842238474
Pouyan <popeno2003@gmail.com>
2014-11-20 10:40:27 +0100

    added stuff

---
+++ src/jpp/numbergame/gui/TileGridPane.java
@@ -0,0 +1,37 @@
+package jpp.numbergame.gui;
+
+import javafx.beans.binding.DoubleBinding;
+import javafx.beans.property.ReadOnlyDoubleProperty;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import jpp.numbergame.Tile;
+
+public class TileGridPane extends Pane {
+
+	private int width;
+	private int height;
+	private Tile[][] tList;
+	private DoubleBinding db;
+
+	public TileGridPane(int width, int height) {
+		this.width = width;
+		this.height = height;
+		Tile[][] tList = new Tile[this.width][this.height];
+		final ReadOnlyDoubleProperty widthProp = this.widthProperty();
+		db = new DoubleBinding() {
+			@Override
+			protected double computeValue() {
+				return (widthProp.doubleValue() / 400) + 1;
+			}
+		};
+		for (int i = 0; i < this.width; i++) {
+			Line ln = new Line();
+			ln.setFill(Color.GRAY);
+			ln.strokeWidthProperty().bind(db);
+			ln.setStartY(0);
+			ln.endYProperty().bind(this.heightProperty());
+		}
+	}
+
+}
